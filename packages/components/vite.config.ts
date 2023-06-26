977811c2e1e3db4d6762d0ded5a805c1b2240a33
Your Name <you@example.com>
2023-06-26 14:43:04 +0800

    feat: 调整components打包产物

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
           //打包格式
           format: "es",
           //打包后文件名
-          entryFileNames: "[name].mjs",
+          entryFileNames: "[name].js",
           //让打包目录和我们目录对应
           preserveModules: true,
           exports: "named",
