16316c90422c000b99064a62345eb2f5945bb37c
Your Name <you@example.com>
2023-06-28 10:23:27 +0800

    feat: add vite-plugin-vue-devtools

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import DefineOptions from "unplugin-vue-define-options/vite";
 import { visualizer } from "rollup-plugin-visualizer";
+import vueDevtools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), DefineOptions(), visualizer({ open: true })],
+  plugins: [vue(), DefineOptions(), visualizer({ open: true }), vueDevtools()],
 });
